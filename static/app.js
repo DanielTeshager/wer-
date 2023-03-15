f902eec6d9530c5e3907dfc339ae0da5d08142f0
Daniel <daniel.wolela@gmail.com>
2023-03-15 21:44:22 +0400

    adjust viewport to zoom out after message on mobiles

--- static/app.js
+++ static/app.js
@@ -1,8 +1,13 @@
 const sendButton = document.querySelector("#send-button");
+const viewport = document.getElementById("viewport");
+viewport.setAttribute(
+	"content",
+	"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"
+);
 
 sendButton.addEventListener("click", (e) => {
 	e.preventDefault();
-	// Get the message from the input field and add it to the chat
+
 	const message = document.querySelector("#chat-input").value;
 	const messageElement = document.createElement("div");
 	messageElement.classList.add("message");
@@ -10,41 +15,26 @@ sendButton.addEventListener("click", (e) => {
 	messageElement.innerHTML = message;
 	document.querySelector("#chat-messages").appendChild(messageElement);
 
-	// show loading... text on the input field
-	// disable the message input field
-	document.querySelector("#chat-input").value = "Loading...";
-	document.querySelector("#chat-input").disabled = true;
-
-	// Send the message to the Python Flask server using fetch
-	fetch("/get-response", {
+	fetch("http://127.0.0.1:50002/get-response", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({ message: message }),
 	})
-		// Get the response from the Python Flask server
 		.then((response) => response.json())
 		.then((data) => {
-			// Hide the loading animation
-			// disable the message input field
-			document.querySelector("#chat-input").value = "";
-			document.querySelector("#chat-input").disabled = false;
-
 			const messageElement = document.createElement("div");
 			messageElement.classList.add("message");
 			messageElement.classList.add("message--received");
 			messageElement.innerHTML = data.response;
 			document.querySelector("#chat-messages").appendChild(messageElement);
 
-			// scroll to the bottom of the chat smoothly
-			chatMessages.scrollTo({
-				top: chatMessages.scrollHeight,
-				behavior: "smooth",
-			});
-
-			//focus on the input field
-			document.querySelector("#chat-input").focus();
+			const viewport = document.getElementById("viewport");
+			viewport.setAttribute(
+				"content",
+				"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"
+			);
 		});
 });
 
